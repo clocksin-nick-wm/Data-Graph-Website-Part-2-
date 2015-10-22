856fcc901c76f203ce20833732ccf2e48ecfd3c3
Start Coding Student 1 <session1@start-coding-mbp-c02pp71ug8wn.local>
2015-10-22 15:25:07 -0700

    Added description

---
+++ Photo Description.js
@@ -0,0 +1,36 @@
+function img1Desc() {
+    var img1Desc = prompt("What shall you change the text to sire?");
+    $("#Description1").html(img1Desc);
+}
+function img2Desc() {
+    var img2Desc= prompt("You want to change this AWESOME text?");
+    $("#Desc2").html(img2Desc);
+}
+function img3Desc() {
+    var img3Desc= prompt("Change the text you must?");
+    $("#Desc3").html(img3Desc);
+}
+function img4Desc() {
+    var img4Desc= prompt("May the force guide you in changing this text");
+    $("#Desc4").html(img4Desc);
+}
+function img5Desc() {
+    var img5Desc= prompt("Will you or won't you ruin this text?");
+    $("#Desc5").html(img5Desc);
+}
+function img6Desc() {
+    var img6Desc= prompt("Change must come to this text?");
+    $("#Desc6").html(img6Desc);
+}
+function img7Desc() {
+    var img7Desc= prompt("Just Kidding No Game");
+    $("#Desc7").html(img7Desc);
+}
+function img8Desc() {
+    var img8Desc= prompt("STOP CHANGING THE TEXT");
+    $("#Desc8").html(img8Desc);
+}
+function img9Desc() {
+    var img9Desc= prompt("You win!!!!");
+    $("#Desc9").html(img9Desc);
+}
